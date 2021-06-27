6104c0d6e25b80c6445af5cb816928b46f68f999
hunglthe150135 <hunglthe150135@fpt.edu.vn>
2021-06-27 23:35:21 +0700

    read csv & data structure hunglt

--- histogram_01.py
+++ histogram_01.py
@@ -1,5 +1,17 @@
-from hieunm import histogram
+import csv
+class input:
+  def __init__(self):
+      self.data = {
+          'id': [],
+          'mas291': []
+      }
 
-print("Hello World")
+      with open("SAMPLE_DATA_01.csv", "r", encoding="utf_8") as file:
+          csv_reader = csv.DictReader(file)
+          for row in csv_reader:
+              self.data['id'].append(row['ID'])
+              self.data['mas291'].append(float(row['MAS291']))
 
-histogram()
+input_sample_data_01 = input()
+print(input_sample_data_01.data)
+
