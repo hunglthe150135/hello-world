4d6c5bac9dd6e05ff5dff92401dd76770c309076
NgoHieu <hieunmhe150483@fpt.edu.vn>
2021-06-27 22:51:28 -0700

    Redone homework

--- histogram_01.py
+++ histogram_01.py
@@ -1,11 +1,5 @@
+from hieunm import histogram
+
 print("Hello World")
 
-import matplotlib.pyplot as plt
-probability = [0.25127, 0.17005, 0.14721, 0.23465, 0.19682]
-point = ['0-2', '2.2-4', '4.2-6', '6.2-8', '8.2-10'] #sample names
-plt.bar(point, probability)
-plt.xticks(point)
-plt.yticks(probability) 
-plt.xlabel('Point')
-plt.ylabel('Probability')
-plt.show()
+histogram()
