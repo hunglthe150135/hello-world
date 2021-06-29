5e0100e0a7452637ccdd73c85e75ceb2f4cc506b
daidh152001 <daidh152001@gmail.com>
2021-06-29 16:43:15 +0700

    dai upload create distribution

--- histogram_01.py
+++ histogram_01.py
@@ -1,7 +1,10 @@
 import csv
 import pandas as pd
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, pyplot
 import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+
 class input:
   def __init__(self):
       self.data = {
@@ -22,9 +25,9 @@ print(input_sample_data_01.data)
 
 plt.style.use('fivethirtyeight')
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('SAMPLE_DATA_01.csv')
 ids = data['ID']
-grades = data['Grade']
+grades = data['MAS291']
 
 bins = [0,2.2,4.4,6.6,8.8,10]
 
@@ -51,3 +54,32 @@ plt.title('bài ca fuckboy')
 plt.ylabel('frequency')
 plt.xlabel('Score')
 plt.show()
+
+
+k = np.array([0,2.2,4.4,6.6,8.8,10])
+mylabels = [0,2.2,4.4,6.6,8.8,10]
+
+plt.pie(k,labels=mylabels,startangle=90)
+plt.show()
+
+reports=[]
+with open('SAMPLE_DATA_01.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        masv = row['ID']
+        mark = row['MAS291']
+        reports.append({'masv': masv,
+                        'mark': mark})
+
+distributions = [float(mark['mark']) for mark in reports]
+counter = Counter(distributions)
+number_of_marks = []
+distributions_of_mark = []
+for key in counter.keys():
+    distributions_of_mark.append(key)
+    number_of_marks.append(counter.get(key))
+
+pyplot.barh(number_of_marks,distributions_of_mark,
+           color='blue',
+           label='Distribution of marks')
+pyplot.show()
