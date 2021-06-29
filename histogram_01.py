07976bc55b82f17943d173c6d2dd8a809c816d1a
NhatLinh <linhplnhe150512@fpt.edu.vn>
2021-06-29 16:12:24 +0700

    fixed code

--- histogram_01.py
+++ histogram_01.py
@@ -22,7 +22,7 @@ print(input_sample_data_01.data)
 
 plt.style.use('fivethirtyeight')
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data.SAMPLE_DATA_01')
 ids = data['ID']
 grades = data['Grade']
 
